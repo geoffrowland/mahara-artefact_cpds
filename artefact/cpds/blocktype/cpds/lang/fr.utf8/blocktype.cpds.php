2cae0e8b44273895028b5ceeb707f3e3ba0e5da7
Dominique-Alain JAN <djanmac.com>
2013-12-28 16:57:14 +0100

    Added the fr.utf8 files

---
+++ artefact/cpds/blocktype/cpds/lang/fr.utf8/blocktype.cpds.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage artefact-cpds
+ * @author     James Kerrigan
+ * @author     Geoffrey Rowland 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 James Kerrigan and Geoffrey Rowland geoff.rowland@yeovil.ac.uk
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Votre PDP';
+$string['description'] = 'Affiche la liste de vos PDP';
+$string['defaulttitledescription'] = 'Si cettre rubrique est laissée vide, le nom du PDP sera utilisé comme titre.';
+$string['newercpds'] = 'Nouveau PDP';
+$string['nocpdsaddone'] = 'Aucun PDP pour l\'instant. %sAjoutez-en un%s !';
+$string['oldercpds'] = 'Ancien PDP';
+$string['cpdstoshow'] = 'PDP à afficher';
+?>
